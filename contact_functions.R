b5cc54f9801a32fdf3f5a8e0dbfe5de290422aab
rchaput <rchaput.pro@gmail.com>
2023-01-03 20:44:21 +0100

    Add helper functions to print contact info
    Contact icons rely on FontAwesome and AcademicIcons; they must be available in some way (either packaging the files or adding the `<link>` tag).
    
    Note: the functions use `<i>` tags for icons. This is perhaps not optimal, but is still a standard way.

---
+++ contact_functions.R
@@ -0,0 +1,57 @@
+# Helper methods to print contact information.
+
+cv.contact <- function (icon, url, label = "", alt = "") {
+    template <- "<a href='{url}' alt='{alt}' title='{alt}' target=_blank><i class='{icon}'></i>{label}</a>"
+    print(glue::glue(template))
+}
+
+cv.contact.home <- function (url, label = "") {
+    cv.contact("fa fa-home", url, label, alt = "Homepage")
+}
+
+cv.contact.github <- function (username, show_username = FALSE) {
+    url <- paste0("https://github.com/", username, "/")
+    label <- if (show_username) username else ""
+    cv.contact("fa fa-github", url, label, alt = "GitHub account")
+}
+
+cv.contact.email <- function (address, show_address = FALSE) {
+    url <- paste0("mailto:", address)
+    label <- if (show_address) address else ""
+    cv.contact("fa fa-envelope", url, label, alt = "E-Mail")
+}
+
+cv.contact.twitter <- function (username, show_username = FALSE) {
+    url <- paste0("https://twitter.com/", username)
+    label <- if (show_username) username else ""
+    cv.contact("fa fa-twitter", url, label, alt = "Twitter")
+}
+
+cv.contact.orcid <- function (orcid, show_orcid = FALSE) {
+    url <- paste0("https://orcid.org/", orcid)
+    label <- if (show_orcid) orcid else ""
+    cv.contact("fab fa-orcid", url, label, alt = "ORCID")
+}
+
+cv.contact.linkedin <- function (username, show_username = FALSE) {
+    url <- paste0("https://www.linkedin.com/in/", username)
+    label <- if (show_username) username else ""
+    cv.contact("fab fa-linkedin", url, label, alt = "Linkedin")
+}
+
+cv.contact.researchgate <- function (username, show_username = FALSE) {
+    url <- paste0("https://www.researchgate.net/profile/", username)
+    label <- if (show_username) username else ""
+    cv.contact("ai ai-researchgate", url, label, alt = "Research Gate")
+}
+
+cv.contact.zotero <- function (username, show_username = FALSE) {
+    url <- paste0("https://www.zotero.org/", username)
+    label <- if (show_username) username else ""
+    cv.contact("ai ai-zotero", url, label, alt = "Zotero")
+}
+
+cv.contact.dblp <- function (username) {
+    url <- paste0("https://dblp.uni-trier.de/pid/", username, ".html")
+    cv.contact("ai ai-dblp", url, alt = "DBLP")
+}
